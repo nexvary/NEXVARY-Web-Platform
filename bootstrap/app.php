5ad3d047c93fb696861086122c0b22163c0cdbd7
nexvary <alibabo@tutamail.com>
2026-09-06 13:14:04 -0700

    security: wire CSP nonce and admin authorization middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Middleware\EnsureAdmin;
 use App\Http\Middleware\SecurityHeaders;
 use App\Http\Middleware\SetLocale;
+use App\Http\Middleware\UseCspNonce;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +15,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [SetLocale::class, SecurityHeaders::class]);
+        $middleware->alias([
+            'admin' => EnsureAdmin::class,
+        ]);
+
+        $middleware->web(append: [
+            SetLocale::class,
+            UseCspNonce::class,
+            SecurityHeaders::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Central exception reporting hooks are added in the observability phase.
