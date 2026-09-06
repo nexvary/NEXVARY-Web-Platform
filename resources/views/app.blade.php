762ec3caed7a44cc0d4e7e6a95b3d9d499a00250
nexvary <alibabo@tutamail.com>
2026-09-06 14:40:04 -0700

    feat(brand): wire viewport-safe favicon metadata

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="theme-color" content="#020817">
+    <meta name="color-scheme" content="dark">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <title inertia>{{ config('app.name', 'NEXVARY') }}</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
