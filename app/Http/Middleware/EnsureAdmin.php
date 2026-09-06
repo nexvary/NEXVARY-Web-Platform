837925fd8ec652c67e02757806ebc9282636d60e
nexvary <alibabo@tutamail.com>
2026-09-06 13:13:34 -0700

    security: require explicit admin authorization

---
+++ app/Http/Middleware/EnsureAdmin.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class EnsureAdmin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        abort_unless((bool) $request->user()?->is_admin, 403);
+
+        return $next($request);
+    }
+}
