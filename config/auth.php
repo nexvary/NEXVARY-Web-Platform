a396eb3a5fdfc9dc9fad137ee0c4fb23a0e5368d
nexvary <alibabo@tutamail.com>
2026-09-07 10:19:33 -0700

    Fix Pint strict type qualification in auth config

--- config/auth.php
+++ config/auth.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Models\User;
+
 return [
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
@@ -18,7 +20,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => env('AUTH_MODEL', User::class),
         ],
     ],
 
