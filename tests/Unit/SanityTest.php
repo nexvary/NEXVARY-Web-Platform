9f5fdaa0e535b7aaca9c400a8616987231f440be
nexvary <alibabo@tutamail.com>
2026-09-07 10:21:56 -0700

    Add unit test suite placeholder for PHPUnit

---
+++ tests/Unit/SanityTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class SanityTest extends TestCase
+{
+    public function test_test_suite_bootstraps(): void
+    {
+        self::assertTrue(true);
+    }
+}
