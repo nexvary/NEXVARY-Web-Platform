730737e53927896aba0f97bca6d8e9edf4a687a6
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-08 03:46:24 -0700

    Register public App Center catalog route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: [
             __DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/app-public.php',
             __DIR__.'/../routes/our-work.php',
             __DIR__.'/../routes/contact.php',
             __DIR__.'/../routes/app-center.php',
